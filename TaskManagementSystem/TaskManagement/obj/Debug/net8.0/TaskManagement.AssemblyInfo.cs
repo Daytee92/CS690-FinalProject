8305ed8dae9fdc3f6b9496c68f51d5ac39b7e7d1
Daytee92 <147367807+Daytee92@users.noreply.github.com>
2025-04-06 05:06:37 +0000

    Changes made by program

--- TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c18802c79604a1270ca9e124ee947088bab8e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5439da7e93474e4e8226bf47a6c313d90d762665")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
