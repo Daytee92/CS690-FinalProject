914336f0773db8bf6d6f7f3e373f1a156a7ccf66
Daytee92 <147367807+Daytee92@users.noreply.github.com>
2025-04-13 06:38:26 +0000

    To merge

--- TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08891e10ed6245310fbcb5f1a03561323f040c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089446f35c74a0818cde2f2d080a2bc025dc8054")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
