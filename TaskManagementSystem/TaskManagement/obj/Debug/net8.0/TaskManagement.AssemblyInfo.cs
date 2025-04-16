b29f32d3bca05aad0240b5a8d7ff3b3c0ae9f191
Daytee92 <msdaytee@gmail.com>
2025-04-15 22:26:55 -0700

    Retrying changes

--- TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagementSystem/TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9ad5be1582fb8b7dda2e15eb0db29255922de4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41048c74ccb9258f21030f3c988b39ceb0d54532")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
